07c71291390930789d8f1b39e08fe330bb61f342
hassanbhai5559-lgtm <hassanbhai5559@gmail.com>
2026-07-24 13:30:18 +0500

    Update reader.js

--- reader.js
+++ reader.js
@@ -10,7 +10,7 @@
 =========================*/
 
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.5.136/pdf.worker.min.js";
+"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
 
 /*=========================
  URL PARAMETERS
